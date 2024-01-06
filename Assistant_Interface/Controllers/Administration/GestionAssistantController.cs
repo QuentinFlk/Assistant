ec993f0e46a32689e92bf865ba8cf752044e105b
Quentin CHAFFOIS <quentin.chaffois@francelink.fr>
2024-01-06 12:00:50 +0100

    ajout d'autres élément d'identity

---
+++ Assistant_Interface/Controllers/Administration/GestionAssistantController.cs
@@ -0,0 +1,35 @@
+﻿using Assistant_Bdd.Data;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using NLog.Web;
+
+namespace Assistant_Interface.Controllers.Administration
+{
+    public class GestionAssistantController : Controller
+    {
+        private readonly AssistantContext _accessBddContext;
+        private readonly IConfiguration _configuration;
+        protected Logger Logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+        public GestionAssistantController(AssistantContext accessBddContext, IConfiguration configuration)
+        {
+            _accessBddContext = accessBddContext;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.Message);
+                Logger.Error(ex.StackTrace);
+                Logger.Error(ex.InnerException);
+                return RedirectToAction("Accueil", "Home");
+            }
+        }
+    }
+}
---
+++ Assistant_Interface/Controllers/Administration/UpdateAssistantController.cs
@@ -0,0 +1,36 @@
+﻿using Assistant_Bdd.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using NLog.Web;
+
+namespace Assistant_Interface.Controllers.Administration
+{
+    public class UpdateAssistantController : Controller
+    {
+        private readonly AssistantContext _accessBddContext;
+        private readonly IConfiguration _configuration;
+        protected Logger Logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+        public UpdateAssistantController(AssistantContext accessBddContext, IConfiguration configuration)
+        {
+            _accessBddContext = accessBddContext;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.Message);
+                Logger.Error(ex.StackTrace);
+                Logger.Error(ex.InnerException);
+                return RedirectToAction("Accueil", "Home");
+            }
+        }
+    }
+}
